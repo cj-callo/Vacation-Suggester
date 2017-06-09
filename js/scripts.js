672b7ef280135d54f31247ec293edb3df75c4b14
Christine Callo <christinejoann.callo@gmail.com>
2017-06-09 01:36:33 -0700

    Added inital script variables and added button on html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
-  var flavor = $("input:radio[name=type]:checked").val();
-  var flavor = $("input:radio[name=action]:checked").val():
-}
+  $("form#survey").submit(function(event) {
+  var  personality = $("input:radio[name=type]:checked").val();
+  var ideal = $("input:radio[name=action]:checked").val();
+  var schedule = $("input:radio[name=time]:checked").val();
+  var item = $("input:radio [name=object]:checked").val();
+  var food = $("input:radio[name=cusine]:checked").val();
+  var people = $("input:radio[name=meet]:checked").val();
+  var shoes = $("input:radio[name=comfortable]:checked").val();
+
+  var nation ="";
+
+  if ()
+
+
+
+
+
+
+
+// Possible countries to visit and attributes
+// Nepal - peace-lover, mysterious, somewhere in between, book, a few, barefoot
+//
