3fabfb93718f08c53cbfda775879f27b75c67778
Christine Callo <christinejoann.callo@gmail.com>
2017-06-09 07:38:10 -0700

    Initiated branching on scripts and set-up of 'if' condition

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,28 @@ $(document).ready(function() {
   var people = $("input:radio[name=meet]:checked").val();
   var shoes = $("input:radio[name=comfortable]:checked").val();
 
-  var nation ="";
+  var place ="";
 
-  if ()
+//  if (characteristic === "bravery" && color === "scarlet") {
+//    house = "Griffindor";
+//  } else if (characteristic === "bravery" && animal === "lion") {
+  //  house = "Griffindor";
+
+  if (personality === "risk" && ideal === "adventurous" && people === "few" && shoes === "hiking") {
+      place = "Annapurna, Nepal";
+    } else if (personality === "peaceful" && ideal === "mysterious" && item === "book" && food === "veggies") {
+      place = "Goa, India";
+      else if
+    } else {
+      place = "Stay home"
+    }
+
+
+    $("#place").text(place);
+    $("#result").show();
+  event.preventDefault();
+});
+});
 
 
 
