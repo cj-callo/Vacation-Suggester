74976cb5e93353f2cdb176494871a9096421c33c
Christine Callo <christinejoann.callo@gmail.com>
2017-06-11 11:11:51 -0700

    Added background color in #F6F7C6

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-  var  personality = $("input:radio[name=type]:checked").val();
-  var ideal = $("input:radio[name=action]:checked").val();
-  var schedule = $("input:radio[name=time]:checked").val();
-  var item = $("input:radio[name=tool]:checked").val();
-  var food = $("input:radio[name=cusine]:checked").val();
-  var people = $("input:radio[name=meet]:checked").val();
-  var shoes = $("input:radio[name=comfortable]:checked").val();
+    var  personality = $("input:radio[name=type]:checked").val();
+    var ideal = $("input:radio[name=action]:checked").val();
+    var schedule = $("input:radio[name=time]:checked").val();
+    var item = $("input:radio[name=tool]:checked").val();
+    var food = $("input:radio[name=cusine]:checked").val();
+    var people = $("input:radio[name=meet]:checked").val();
+    var shoes = $("input:radio[name=comfortable]:checked").val();
 
-  var place ="";
+    var place ="";
 
-  if (personality === "risk" && ideal === "adventurous" && people === "few" && shoes   === "hiking" && item === "kit") {
+    if (personality === "risk" && ideal === "adventurous" && people === "few" && shoes   === "hiking" && item === "kit") {
       place = "Kilimanjaro, Tanzania";
       $("#tanzania").show();
     } else if (personality === "peaceful" && ideal === "mysterious" && food === "veggies") {
@@ -20,55 +20,55 @@ $(document).ready(function() {
       place = "London, UK";
       $("#london").show();
     } else if (personality === "fun" && food === "drinks" && people === "lot") {
-    place = "Rio de Janeiro, Brazil";
+      place = "Rio de Janeiro, Brazil";
       $("#rio").show();
-  } else if (personality === "risk" && food === "meats" && people === "lot") {
-    place = "Berlin, Germany";
-    $("#berlin").show();
-  } else if (personality === "peaceful" && ideal === "relaxing" && (shoes === "flip" || shoes === "barefoot")) {
-    place = "Motu Teta, Tahiti";
-    $("#tahiti").show();
-  } else if (personality === "busy" && people === "lot" && item === "camera") {
-    place = "Tokyo, Japan";
-    $("#tokyo").show();
-} else if (item === "makeup" && food === "restaurants" && shoes === "dress") {
-    place = "Paris, France";
-    $("#paris").show();
-} else if (ideal = "adventurous" && item === "camera" && food === "veggies") {
-    place = "Tropical forests of Costa Rica";
-    $("#costa").show();
-  } else if (personality === "risk" && people === "lot") {
-    place = "Bangkok, Thailand";
-    $("#bangkok").show();
-  } else if (personality === "risk" && people === "few") {
-    place = "Nunavut, Canada";//check spelling
-    $("#nunavut").show();
-  } else if (personality === "peaceful" && people === "lot") {
-    place = "Great Wall of China";
-    $("china").show();
-  } else if (personality === "busy" && people === "few") {
-    place = "Dubai, United Arab Emirates";
-    $("#dubai").show();
-  } else if (personality === "fun" && people === "few") {
-    place = "Stockholm, Sweden";
-    $("#stockholm").show();
-  } else if (ideal === "adventurous" && people === "few") {
+    } else if (personality === "risk" && food === "meats" && people === "lot") {
+      place = "Berlin, Germany";
+      $("#berlin").show();
+    } else if (personality === "peaceful" && ideal === "relaxing" && (shoes === "flip" || shoes === "barefoot")) {
+      place = "Motu Teta, Tahiti";
+      $("#tahiti").show();
+    } else if (personality === "busy" && people === "lot" && item === "camera") {
+      place = "Tokyo, Japan";
+      $("#tokyo").show();
+    } else if (item === "makeup" && food === "restaurants" && shoes === "dress") {
+      place = "Paris, France";
+      $("#paris").show();
+    } else if (ideal = "adventurous" && item === "camera" && food === "veggies") {
+      place = "Tropical forests of Costa Rica";
+      $("#costa").show();
+    } else if (personality === "risk" && people === "lot") {
+      place = "Bangkok, Thailand";
+      $("#bangkok").show();
+    } else if (personality === "risk" && people === "few") {
+      place = "Nunavut, Canada";//check spelling
+      $("#nunavut").show();
+    } else if (personality === "peaceful" && people === "lot") {
+      place = "Great Wall of China";
+      $("china").show();
+    } else if (personality === "busy" && people === "few") {
+      place = "Dubai, United Arab Emirates";
+      $("#dubai").show();
+    } else if (personality === "fun" && people === "few") {
+      place = "Stockholm, Sweden";
+      $("#stockholm").show();
+    } else if (ideal === "adventurous" && people === "few") {
       place = "Easter Island";
-    $("#easter").show();
+      $("#easter").show();
     } else if (ideal === "relaxing" && people === "lot") {
       place = "Cancun, Mexico";
-    $("#cancun").show();
+      $("#cancun").show();
     } else if (ideal === "educational" && people === "lot") {
       place = "Rome, Italy";
-    $("#rome").show();
+      $("#rome").show();
     } else if (ideal === "educational" && people === "few") {
       place = "Muiden, Neatherlands"
-    $("#muiden").show();
+      $("#muiden").show();
     } else if (ideal === "mysterious" && people === "lot") {
       place = "Machu Pechu, Peru";
-    $("#peru").show();
+      $("#peru").show();
     } else if (item === "camera" && people === "lot") {
-       place = "Singapore";
+      place = "Singapore";
       $("#singapore").show();
     } else if (shoes === "flip") {
       place = "Maui, Hawaii";
@@ -81,12 +81,12 @@ $(document).ready(function() {
       $("#cottage").show();
     }
 
-  $("#place").text(place);
-  $("#result").show();
+    $("#place").text(place);
+    $("#result").show();
 
-  //blank form submission functions not working, taken out, need to fix
+    //blank form submission functions not working, taken out, need to fix
 
-  event.preventDefault();
+    event.preventDefault();
 
   });
 });
